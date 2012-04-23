2cc84ac81caf71ace988dea213aaa78cbfd0a90d
Deiu <andrei@fcns.eu>
2012-04-23 23:09:09 +0200

    This file now correctly handles crossdomain incoming pingbacks.

--- notification.php
+++ notification.php
@@ -1,77 +1,97 @@
 <?php
-/*
- *  Copyright (C) 2012 MyProfile Project
- *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal 
- *  in the Software without restriction, including without limitation the rights 
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
- *  copies of the Software, and to permit persons to whom the Software is furnished 
- *  to do so, subject to the following conditions:
+//-----------------------------------------------------------------------------------------------------------------------------------
+//
+// Filename   : pingback.php
+// Date       : 21st Apr 2011
+//
+// Project name: SemPB - Semantical Pingback
+// Copyright 2011 fcns.eu
+// Author: Andrei Sambra - andrei@fcns.eu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// See <http://www.gnu.org/licenses/> for a description of this license.
 
- *  The above copyright notice and this permission notice shall be included in all 
- *  copies or substantial portions of the Software.
-
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
- *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
- *  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
- *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
- *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
- *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
- 
-/*
- * send a notification message
- */ 
-
-//phpinfo(INFO_VARIABLES);
-require_once 'include.php'; 
-
-// verify if we're logged in or not
-check_auth($idp, $page_uri);
+define('INCLUDE_CHECK',true);
+require 'lib/graphite.php';
+require 'lib/arc/ARC2.php';
+require 'config.php';
 
 $ret = "";
-$ret .= "<div class=\"container\">\n";
-// save
-if ((isset($_REQUEST['doit'])) && (isset($_REQUEST['to']))) {
-    $from   = mysql_real_escape_string($_SESSION['webid']);
-    $to     = mysql_real_escape_string($_REQUEST['to']);
-    $msg    = mysql_real_escape_string($_REQUEST['message']);
-    $hash   = mysql_real_escape_string($_SESSION['user_hash']);
-    $name   = mysql_real_escape_string($_SESSION['usr']);
-    $pic    = mysql_real_escape_string($_SESSION['img']);
 
-    // write webid uri to database
-    $query = "INSERT INTO pingback_messages SET date='" . time() . "', from_uri='" . $from . "', to_hash='" . $hash . "', to_uri='" . $to . "', name='" . $name . "', pic='" . $pic . "'";
-    if (isset($_REQUEST['message']))
-    $query .= ", msg = ' " . $msg . "'";
-    $result = mysql_query($query);
-    if (!$result) {
-        $ret .= error('SQL Error!');
-    } else {
-        $ret .= success('Your notification has been successfully delivered!');
+// process form and send pingback
+if (isset($_POST['source'])) {
+
+    $from   = mysql_real_escape_string(trim($_REQUEST['source']));
+    $to     = mysql_real_escape_string(trim($_REQUEST['target']));
+    $msg    = mysql_real_escape_string($_REQUEST['comment']);
+
+    // fetch the user's profile
+    $fg = new Graphite();
+    $fg->load($from);
+    $fr = $fg->resource($from);
+
+    $ok = 0;
+    // only send pings if the destination person is friends with the sender (avoids spam)
+    foreach($fr->all("foaf:knows") as $friend) {
+        // check if the target is my friend or not       
+        if ($friend == $to) {
+            // write webid uri to database
+            $query = "INSERT INTO pingback_messages SET date='" . time() . "', from_uri = '" . $from . "', to_uri = '" . $to . "'";
+            if (isset($_REQUEST['comment']))
+                $query .= ", msg = ' " . $msg . "'";
+            $result = mysql_query($query);
+            if (!$result) {
+                $ret  = 'Invalid query: ' . mysql_error() . "\n";
+                $ret .= 'Query: ' . $query;
+            } else {
+                $ret .= header("HTTP/1.1 201 Created");
+                $ret .= header("Status: 201 Created");
+                $ret .= "<html><body>\n";
+                $ret .= "Your notification has been successfully delivered!\n";
+                $ret .= "</body></html>\n";
+            }
+            // everything is ok, exit loop
+            $ok = 1;
+            break;
+        }
     }
+    // return a proper HTTP response code with error
+    if ($ok == 0) {
+        $ret .= header("HTTP/1.1 404 Not Found");
+        $ret .= header("Status: 404 Not Found");
+        $ret .= "<html><body>\n";
+        $ret .= "Could not find any mention of <strong>" . $to . "</strong> in your list of known people.\n";
+        $ret .= "</body></html>\n";
+    }
+} else {
+    // show form
+    $ret .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">';
+    $ret .= "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"  xmlns:pingback=\"http://purl.org/net/pingback/\">\n";
+    $ret .= "   <head>\n";
+    $ret .= "	<title>Pingback</title>\n";
+    $ret .= "	<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />\n";
+    $ret .= "   </head>\n";
+    $ret .= "   <body typeof=\"pingback:Container\">\n";
+    $ret .= "   <form method=\"post\" action=\"http://fcns.eu/people/andrei/pingback.php\">\n";
+    $ret .= "       <p>Your WebID: <input size=\"30\" property=\"pingback:source\" type=\"text\" name=\"source\" /></p>\n";
+    $ret .= "       <p>Target WebID: <input size=\"30\" property=\"pingback:target\" type=\"text\" name=\"target\" value=\"" . $_GET['target'] . "\" /></p>\n";
+    $ret .= "       <p>Comment (optional): <input size=\"30\" maxlength=\"256\" type=\"text\" name=\"comment\" style=\"background-color:#fff; border:dashed 1px grey;\" /></p>\n";
+    $ret .= "       <p><input type=\"submit\" name=\"submit\" value=\"Ping!\" /></p>\n";
+    $ret .= "   </form>\n";
+    $ret .= "   </body>\n";
+    $ret .= "</html>\n";
 }
 
-// show form
-
-$ret .= "<p><font style=\"font-size: 2em; text-shadow: 0 1px 1px #cccccc;\">Send a notification</font></p><br/>\n";
-$ret .= "<p>Notifications only work for people which have subscribed to it locally.</p><br/>\n";
-$ret .= "<form name=\"send\" method=\"POST\" action=\"\">\n";
-$ret .= "<input type=\"hidden\" name=\"doit\" value=\"1\">\n";
-$ret .= "<table border=\"0\">\n";
-$ret .= "<tr valign=\"top\"><td>From WebID: <br/>&nbsp;</td><td><strong>You</strong> <font color=\"grey\"><small>(" . $_SESSION['webid'] . ")</small></font></td></tr>\n";
-$ret .= "<tr valign=\"top\"><td>Target WebID: <br/>&nbsp;</td><td><input size=\"30\" type=\"text\" name=\"to\" value=\"" . $_REQUEST['to'] . "\"></td></tr>\n";
-$ret .= "<tr valign=\"top\"><td>Short message (256): <br/>(optional)</td><td> <textarea style=\"height: 130px;\" name=\"message\"></textarea></td></tr>\n";
-$ret .= "<tr><td><br/><input type=\"submit\" class=\"button ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only\" name=\"submit\" value=\" Send notification! \"></td><td></td></tr>\n";
-$ret .= "</table>\n";
-$ret .= "</form>\n";
-
-$ret .= "<div class=\"clear\"></div>\n";
-$ret .= "</div>\n";
-
-include 'header.php';
 echo $ret;
-include 'footer.php';
 
-?>
+?>		      
+
