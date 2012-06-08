1ce746752b504b62dcb8a75f4b79fa0c6fdae4a9
deiu <andrei@fcns.eu>
2012-06-08 19:07:02 +0200

    Users can now specify a SPARQL endpoint; useful for caching and local search.

--- install.php
+++ install.php
@@ -221,6 +221,9 @@ if (isset($_REQUEST['submit'])) {
         $content .= "/* IDP address */\n";
         $content .= '$idp = \'' . trim($_REQUEST['idp']) . '\';' . "\n";
         $content .= "\n";
+        $content .= "/* SPARQL endpoint */\n";
+        $content .= '$endpoint = \'' . trim($_REQUEST['endpoint']) . '\';' . "\n";
+        $content .= "\n";
         $content .= "/* Database config */\n";
         $content .= '$db_database   = \'' . $db_database . '\';' . "\n";
         $content .= '$db_host       = \'' . $db_host . '\';' . "\n";
@@ -305,8 +308,11 @@ if (isset($_REQUEST['submit'])) {
     $ret .= "<tr><td>OpenSSL config file: </td><td><input type=\"text\" name=\"openssl\" size=\"50\" placeholder=\"/etc/ssl/private/CA.key\" value=\"\"></td></tr>\n";
 
     $ret .= "<tr><td colspan=\"2\"><br/><p><strong>Delegated authentication</strong></p><br/></td></tr>\n";
-    $ret .= "<tr><td>IdP URI: </td><td><input type=\"text\" name=\"idp\" size=\"50\" value=\"https://auth.my-profile.eu/auth/index.php?authreqissuer=\"></td></tr>\n";
+    $ret .= "<tr><td>IdP address: </td><td><input type=\"text\" name=\"idp\" size=\"50\" value=\"https://auth.my-profile.eu/auth/index.php?authreqissuer=\"></td></tr>\n";
     $ret .= "<tr><td colspan=\"2\"><font color=\"grey\">Important note regarding using a different IdP: if you want to use a different IdP, you will have to edit the file <i>lib/libAuthentication/lib/Authentication_X509CertRepo.php</i> and add the IdP's certificate in (PEM form) to the array of IdPs.</font></td></tr>\n";
+
+    $ret .= "<tr><td colspan=\"2\"><br/><p><strong>SPARQL endpoint</strong></p><br/></td></tr>\n";
+    $ret .= "<tr><td>Endpoint address: </td><td><input type=\"text\" name=\"endpoint\" size=\"50\" value=\"\"></td></tr>\n";
     
     $ret .= "<tr><td colspan=\"2\"><br/><p><strong>Database configuration</strong></p><br/></td></tr>\n";
     $ret .= "<tr><td>Database host: </td><td><input type=\"text\" name=\"host\" value=\"localhost\"></td></tr>\n";
