1478b0afdcc962d4f661a352010eb6bcf5aff325
deiu <andrei@fcns.eu>
2012-06-15 10:50:30 +0200

    Revert "Reverted the changes to $page_uri."
    This reverts commit 2e942748378491534f33d0bd1b1514993f2e94b9.

--- include.php
+++ include.php
@@ -57,16 +57,15 @@ require_once 'lib/feeds/FeedWriter.php';
 require_once 'lib/EasyRdf.php';
 require_once 'lib/graphite.php';
 
-// Get the current document URI
-$page_uri = 'http';
+// Get the current document URI (base uri)
+$base_uri = 'http';
 if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == 'on') {
-    $page_uri .= 's';
+    $base_uri .= 's';
 }
-$page_uri .= '://' . $_SERVER['SERVER_NAME'];
-// this is the base uri
-$base_uri = $page_uri;
-// add current document
-$page_uri .= $_SERVER['REQUEST_URI'];
+$base_uri .= '://' . $_SERVER['SERVER_NAME'];
+
+// add current document to form full uri
+$page_uri = $_SERVER['REQUEST_URI'];
 
 // Preparing the session
 session_name('tzLogin');
