c87f1f74f8335050fe58c007b6e53e4263e54cc8
deiu <andrei@fcns.eu>
2012-07-05 16:36:59 +0200

    Updated the autocomplete function for shorthandles. It now displays a picture as well.

--- shorthandle.php
+++ shorthandle.php
@@ -5,9 +5,10 @@ $sparql = sparql_connect(SPARQL_ENDPOINT);
 $input = $_REQUEST["term"];
 $data = array();
 // query the triple store looking for a match to $input
-$query = 'SELECT DISTINCT ?webid, ?name WHERE {
+$query = 'SELECT DISTINCT ?webid, ?name, ?img WHERE {
                     ?webid foaf:name ?name .
                     ?webid foaf:nick ?nick .
+                    ?webid foaf:img ?img .
                     FILTER (regex(?name, "' . $input . '", "i") || regex(?nick, "' . $input . '", "i") || regex(?webid, "' . $input . '", "i"))
                     MINUS { ?webid a foaf:Person .
                            FILTER (regex(?webid, "nodeID", "i")) }
@@ -20,7 +21,15 @@ if(!$result)
     
 while ($row = sparql_fetch_array($result)) {
     $json = array();
-    $json['label'] = $row['name'] . ' (' . $row['webid'] . ')';
+    $json['webid'] = $row['webid'];
+    $json['name'] = $row['name'];
+    $json['img'] = $row['img'];
+    
+    $json['label'] = '<table><tr>';
+    $json['label'] .= '<td><img width="30" src="' . $row['img'] . '"/></td>';
+    $json['label'] .= '<td><strong>' . $row['name'] . '</strong><br/>' . $row['webid'] . '</td>';
+    $json['label'] .= '</tr></table>';
+    
     $json['value'] = '<'. strtolower($row['webid']) . '>';
     $data[] = $json;
 }
@@ -28,4 +37,6 @@ while ($row = sparql_fetch_array($result)) {
 // return data
 header("Content-type: application/json");
 echo json_encode($data);
+
+//echo $data;
 ?>
