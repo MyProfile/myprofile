3585d350f047bddfa3cf58faa32521fd4cfc8ce1
deiu <andrei@fcns.eu>
2013-01-21 11:25:05 +0100

    Autocomplete function now replaces the WebID with the user's name.

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -7,6 +7,7 @@ function extractLast(term) {
     return split(term).pop();
 }
 
+// autocomplete handles when posting to wall
 function do_autocomplete (id, handle) {
     $("#"+id)
         // don't navigate away from the field on tab when selecting an item
@@ -55,3 +56,41 @@ function do_autocomplete (id, handle) {
         });
 }
 
+// autocomplete name for sending messages
+function do_autocomplete_msg (name, to) {
+    $("#"+name)
+        // don't navigate away from the field on tab when selecting an item
+        .bind( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                    $( this ).data( "autocomplete" ).menu.active ) {
+                event.preventDefault();
+            }
+        })
+        .autocomplete({
+            minLength: 0,
+            source: function( request, response ) {
+                var term = request.term;
+                var results = [];
+                $.getJSON( "shorthandle.php", {
+                    term: extractLast( request.term )
+                }, response );
+            },
+            focus: function() {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function( event, ui ) {
+                var terms = split( this.value );
+                // set the WebID in the "to" input
+                $("#"+to).val(ui.item.value);
+                // remove the current input
+                terms.pop();
+                // add the selected item
+                terms.push( ui.item.label );
+                // add placeholder to get the space at the end
+                terms.push( " " );
+                this.value = terms.join( "" );
+                return false;
+            }
+        });
+}
